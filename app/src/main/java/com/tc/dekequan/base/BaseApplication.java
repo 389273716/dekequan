17e041286c6d1bf4a26e7248a684318c8aeb8f17
tc <389273716@qq.com>
2016-09-20 21:34:50 +0800

    加入数据库第三方liteorm 加入工具类

--- app/src/main/java/com/tc/dekequan/base/BaseApplication.java
+++ app/src/main/java/com/tc/dekequan/base/BaseApplication.java
@@ -3,6 +3,8 @@ package com.tc.dekequan.base;
 import android.app.Application;
 
 import com.facebook.stetho.Stetho;
+import com.litesuits.orm.LiteOrm;
+import com.litesuits.orm.db.DataBaseConfig;
 import com.tomtop.ttcom.http.volley.VolleyUtil;
 import com.tomtop.ttcom.image.FrescoHelper;
 import com.tomtop.ttutil.CrashHandler;
@@ -17,6 +19,7 @@ public class BaseApplication extends Application {
         return instance;
     }
 
+    static LiteOrm liteOrm;
 
     @Override
     public void onCreate() {
@@ -33,6 +36,24 @@ public class BaseApplication extends Application {
                         .build());
         LogUtil.init(true);
         VolleyUtil.getInstance().initialize(this);
+
+        initDB();
+    }
+
+    private void initDB() {
+        if (liteOrm == null) {
+            DataBaseConfig config = new DataBaseConfig(this, "dk_quan.db");
+            //"liteorm.db"是数据库名称，名称里包含路径符号"/"则将数据库建立到该路径下，可以使用sd卡路径。 不包含则在系统默认路径下创建DB文件。
+            //例如 public static final String DB_NAME = SD_CARD + "/lite/orm/liteorm.db";
+            // DataBaseConfig config = new DataBaseConfig(this, DB_NAME);
+            config.dbVersion = 1; // set database version
+            config.onUpdateListener = null; // set database update listener
+            //独立操作，适用于没有级联关系的单表操作，
+//            liteOrm = LiteOrm.newSingleInstance(config);
+            //级联操作,适用于多表级联操作
+            liteOrm = LiteOrm.newCascadeInstance(config);
+        }
+        liteOrm.setDebugged(true); // open the log
     }
 
 
